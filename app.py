96bf9c7c074f6fcaad2ee9e5ae990395b8e7fd0d
ppiicckk <117368652+ppiicckk@users.noreply.github.com>
2024-08-21 12:55:37 +0000

    add dynamic data activities

--- app.py
+++ app.py
@@ -1,10 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+activities = [
+  {
+    "id" : 1,
+    "title" : "body",
+    "location" : "Porscarn",
+    "condition" : "1m avec temps couvert et vent vent faible side off, marée haute"
+  },
+  {
+    "id" : 2,
+    "title" : "city stade",
+    "location" : "Combrit",
+    "condition" : "soleil"
+  },
+  {
+    "id" : 3,
+    "title" : "pump track",
+    "location" : "Ploneis",
+    "condition" : "temps couvert"
+  }
+]
+
 @app.route('/')
 def home():
-  return render_template("home.html")
+  return render_template("home.html", ACTIVITIES = activities)
+
+@app.route('/API/activities')
+def lst_activities():
+  return jsonify(activities)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
